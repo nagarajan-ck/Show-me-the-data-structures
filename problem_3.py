7697049c70ac03db2aa869e6c895190926f20206
Nagarajan CK <nagainvertibrate@gmail.com>
2019-07-28 17:05:44 +0530

    code change for test case

--- problem_3.py
+++ problem_3.py
@@ -44,6 +44,7 @@ class Huffman:
         tree = Tree()        
         old_char_freq={}
         sorted_list=list()
+        code=""
         for i in data: 
             if i in old_char_freq: 
                 old_char_freq[i] += 1
@@ -55,9 +56,11 @@ class Huffman:
         
         
         sorted_list= sorted(self.char_freq,key=self.char_freq.get)
-        if(len(self.char_freq.keys())<=1):
+        if(len(self.char_freq.keys())==0):
+        	return code,tree
+        elif(len(self.char_freq.keys())==1):
             tree.set_root(Node(data))
-            code = "0"
+            code = "0"*(len(data))
             return code,tree
         tree = self.create_tree(tree,sorted_list)
         
@@ -101,26 +104,34 @@ class Huffman:
     
     
     def huffman_decoding(self,code,tree):
-        if(len(code)<=1):
+        if(len(code)==0):
+        	   return ""
+        elif(len(code)==1):
             return tree.get_root().value
+        
         self.encode_characters(tree.get_root(),"")
+        
         reversed_encoded_data=dict()
+        
         for key in self.encoded_data:   
             reversed_encoded_data[self.encoded_data.get(key)] = key
-        
         
         decoded_data=""
         temp_str=""
         for i in code:
             temp_str+=i
             if temp_str in reversed_encoded_data:
-                decoded_data+=reversed_encoded_data.get(temp_str).value.value
+                if(reversed_encoded_data.get(temp_str).value.__class__.__name__=='Node'):
+                    decoded_data+=reversed_encoded_data.get(temp_str).value.value
+                else:
+                    decoded_data+=reversed_encoded_data.get(temp_str).value
                 temp_str=""        
         
         return decoded_data
     
     
-    def encode_characters(self,node,code):        
+    def encode_characters(self,node,code):
+        
         if(node is None):          
             return None
             
@@ -140,7 +151,8 @@ class Huffman:
                     self.encode_characters(node.value.left,code+"0")
                 if(node.value.get_right_child()):
                     self.encode_characters(node.value.right,code+"1")
-        
+        elif(node.value.__class__.__name__=='str'):
+            self.encoded_data[node] = "0"*len(node.value)
         
         
 def codeencode(a_great_sentence):
@@ -150,8 +162,10 @@ def codeencode(a_great_sentence):
     print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
     print ("The content of the data is: {}\n".format(a_great_sentence))
     encoded_data, tree  = hufenc.huffman_encoding(a_great_sentence)
-
-    print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    if(encoded_data):
+    	print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    else:
+    	print ("The size of the encoded data is: {}\n".format(encoded_data, base=2))
     print ("The content of the encoded data is: {}\n".format(encoded_data))
 
 
@@ -186,29 +200,30 @@ The size of the data is: 49
 
 The content of the data is: 
 
-The size of the encoded data is: 24
+The size of the encoded data is: 
 
-The content of the encoded data is: 0
+The content of the encoded data is: 
 
 The size of the decoded data is: 49
 
 The content of the encoded data is: 
 '''
 
-codeencode("a")
+codeencode("aaaaa")
 
 '''
-The size of the data is: 50
+The size of the data is: 54
 
-The content of the data is: a
+The content of the data is: aaaaa
 
 The size of the encoded data is: 24
 
-The content of the encoded data is: 0
+The content of the encoded data is: 00000
 
-The size of the decoded data is: 50
+The size of the decoded data is: 54
 
-The content of the encoded data is: a
+The content of the encoded data is: aaaaa
+
 '''
 
 
